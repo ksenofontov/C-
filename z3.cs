96dacd5781c4a01c1194a80bb95a69d7f1c5ce1f
ksenofontov <dennisius@yahoo.com>
2016-10-19 13:36:37 +0300

    Add files via upload

---
+++ z3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace номер_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double j = 0.1;
+            while (j < 1)
+            {
+                Console.WriteLine("X = " + j + " F = " + j + 5);
+                j += 0.2;
+                Console.ReadLine();
+            }
+        }
+    }
+}1
