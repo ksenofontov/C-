f40a5a44ef2e947e4f65e4f09b0d0d283b3e14e9
ksenofontov <dennisius@yahoo.com>
2016-10-19 13:35:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (int j = 1; j < 11; j++)
+                i += j * j;
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
